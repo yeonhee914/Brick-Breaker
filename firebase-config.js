6abc5d708c1dd9040859cc5c74a0fba5a65ecab3
yeonhee914 <5hhh8p9p49@privaterelay.appleid.com>
2026-06-05 15:39:10 +0900

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "Firebase에서 복사한 apiKey",
+  authDomain: "Firebase에서 복사한 authDomain",
+  databaseURL: "https://brick-breaker-ea91f-default-rtdb.firebaseio.com",
+  projectId: "Firebase에서 복사한 projectId",
+  storageBucket: "Firebase에서 복사한 storageBucket",
+  messagingSenderId: "Firebase에서 복사한 messagingSenderId",
+  appId: "Firebase에서 복사한 appId",
+};
